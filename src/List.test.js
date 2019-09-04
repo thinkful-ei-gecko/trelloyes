246814d8534a8b8061d8290a5adba6c142515bff
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-04 12:17:47 -0400

    update List test suite to remove prop error

--- src/List.test.js
+++ src/List.test.js
@@ -9,20 +9,19 @@ describe('test list component', () => {
     ReactDOM.render(
     <List key={2} 
       header='header string' 
-      cards={[{key: 1, title: 'title 1', content: 'content 1'},
-      {key: 2, title: 'title 2', content: 'content 2'}]} 
+      cards={[{id: 1, title: 'title 1', content: 'content 1'},
+      {id: 2, title: 'title 2', content: 'content 2'}]} 
     />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('should match snapshot', () => {
-    let cardsArray = [];
-    cardsArray["key"] = {id: 'a', title: 'First card', content: 'lorem ipsum' };
     const tree = renderer
     .create(
     <List key={2} 
       header='header string' 
-      cards={cardsArray} 
+      cards={[{id: 1, title: 'title 1', content: 'content 1'},
+      {id: 2, title: 'title 2', content: 'content 2'}]} 
     />)
     .toJSON();
     expect(tree).toMatchSnapshot();
